16dcc0e68e15c49f8a36f7c5c72dc7ae9dbe7ae3
monlesliechen <80874123+monlesliechen@users.noreply.github.com>
2021-10-15 22:36:16 +0800

    Add files via upload

---
+++ vega_vis.js
@@ -0,0 +1,14 @@
+var vg_1 = "map_line.json";
+vegaEmbed("#map", vg_1).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "pie.json";
+vegaEmbed("#pie", vg_2).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "bar.json";
+vegaEmbed("#bar", vg_3).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
